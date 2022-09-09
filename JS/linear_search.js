7b1960d4e1b758dd7b908016f753a793c86d9602
Igor Cheribulko <St.codding@gmail.com>
2022-09-09 18:19:56 +0300

    create linear_search

---
+++ JS/linear_search.js
@@ -0,0 +1,15 @@
+const arr = [1, 5, 6, 89, 4, 3, 78, 10];
+let count = 0;
+
+function linearSearch(array, item) {
+	for (let i = 0; i < array.length; i++) {
+		count++;
+		if (array[i] === item) {
+			return console.log(`Индекс искомого элемента равен ${i}. Количество шагов:${count}`);
+		}
+	}
+	return console.log(`Искомый элемент не найден. Количество шагов: ${count}`);
+}
+linearSearch(arr, 7);
+
+// Сложность данного алгоритма О(n), где количество элементов итераций в массиве. 
